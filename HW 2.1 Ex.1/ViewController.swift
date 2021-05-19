de365e941f968043b164a19b433d92df4f8fe1d5
rubis-vladimir <rubis-vladimir>
2021-05-19 18:52:20 +0300

    Hello

---
+++ HW 2.1 Ex.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 2.1 Ex.1
+//
+//  Created by Владимир Рубис on 19.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
